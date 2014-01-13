3456d79d09f35563376d4919c42068779254296c
Jack <xhzengAIB@gmail.com>
2014-01-13 11:07:40 +0000

    Change the projet structure

---
+++ Source/XHWaterDropRefresh.h
@@ -0,0 +1,13 @@
+//
+//  XHWaterDropRefresh.h
+//  XHWaterDropRefresh
+//
+//  Created by 曾 宪华 on 14-1-13.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XHWaterDropRefresh : UIView
+
+@end
