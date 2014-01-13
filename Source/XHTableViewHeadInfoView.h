418218615b15eab230b3f5011648720e05b866b3
Jack <xhzengAIB@gmail.com>
2014-01-13 11:10:11 +0000

    Add TableViewHeadInfoView class

---
+++ Source/XHTableViewHeadInfoView.h
@@ -0,0 +1,13 @@
+//
+//  XHTableViewHeadInfoView.h
+//  XHWaterDropRefresh
+//
+//  Created by 曾 宪华 on 14-1-13.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XHTableViewHeadInfoView : UIView
+
+@end
